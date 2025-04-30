5bd9ab24272d2b8876b9169488585e7b92c19d10
Dimitris <Dimitris.markopoulos@columbia.edu>
2025-04-30 16:00:42 -0400

    fixed bug in processed dataset where apply to target missed . for some cases and caused issues

--- 01_data_preprocessing.py
+++ 01_data_preprocessing.py
@@ -1,45 +1,32 @@
 # ===== import libraries ======
 import pandas as pd
 import numpy as np
-import os
 import matplotlib.pyplot as plt
+import os
 import json
-from sklearn.preprocessing import StandardScaler
 
-# ===== get the columns from the information .txt file ======
-with open("split-data/original/adult.names", "r") as file:
-    contents = file.read()
+columns = [
+    'age', 'workclass', 'fnlwgt', 'education', 'education-num',
+    'marital-status', 'occupation', 'relationship', 'race', 'sex',
+    'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'class'
+]
 
-filtered_content = contents[-1375:].split('\n')
-vars = []
-for var in filtered_content:
-    if var != '':
-        x = var.split(':')[0]
-        vars.append(x)
-vars = vars + ['target']
+df_tr = pd.read_csv('split-data/original/adult.data', names=columns).replace(' ?', np.nan)
+df_ts = pd.read_csv('split-data/original/adult.test', names=columns, skiprows=1).replace(' ?', np.nan)
 
-# ===== import training and testing datasets ======
-params_d = {
-    'names': vars,
-    'header': None,
-    'na_values': ' ?',
-    'skipinitialspace': True
-}
+print(f'Training Data Dimensions: {df_tr.shape} >> (missing = {(df_tr.isna().sum().sum()/(df_tr.shape[0] * df_tr.shape[1]))*100:.3f}%)')
+print(f'Testing Data Dimensions: {df_ts.shape} >> (missing = {(df_ts.isna().sum().sum()/(df_ts.shape[0] * df_ts.shape[1]))*100:.3f}%)')
 
-data_train = pd.read_csv('split-data/original/adult.data', **params_d).replace('?', np.nan)
-data_test = pd.read_csv('split-data/original/adult.test', skiprows=1, **params_d).replace('?', np.nan)
-
-print(f'Training Data Dimensions: {data_train.shape}\nTesting Data Dimensions: {data_test.shape}')
-
-# ===== combine tr and ts into full_data to perform feature engineering (but keep track with col = 'set) ======
-data_train['set'] = 'train'
-data_test['set'] = 'test'
-full_data = pd.concat([data_train, data_test]).reset_index(drop=True)
+df_tr['set'] = 'train'
+df_ts['set'] = 'test'
+full_data = pd.concat([df_tr, df_ts]).reset_index(drop=True)
+full_data['target'] = full_data['class'].apply(lambda x: 1 if x in [' <=50K', ' <=50K.'] else 0)
 
 # ===== viz and clean missing data ======
 perc_missing_df = pd.DataFrame(full_data.isna().sum() / full_data.shape[0]).apply(lambda x: round(100*x,2)).rename(columns={0:'percent_missing'})
 nonzero_missing = perc_missing_df[perc_missing_df['percent_missing'] > 0]
 
+os.makedirs('media', exist_ok=True)
 def plot():
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c']
     plt.figure(figsize=(8, 5))
@@ -56,14 +43,13 @@ def plot():
                     ha='center', va='bottom')
 
     plt.tight_layout()
-    plt.savefig('media/01_media');
-plot()
+    plt.savefig('media/01_viz_missing_data');
 
 # Want to drop all observations with missing data!
 full_data_clean = full_data.dropna(axis=0).reset_index(drop=True)
 dropped_n = full_data.shape[0] - full_data_clean.shape[0]
 dropped_perc = dropped_n / full_data.shape[0]
-print(f'Dropped observations: {dropped_n}\nDropped proportion: {dropped_perc*100:.3f}%')
+print(f'Dropped observations: {dropped_n}\nObservations dropped: {dropped_perc*100:.3f}%')
 
 # ===== one-hot encode ======
 def one_hot_encode(raw_data, col_name_to_encode):
@@ -91,7 +77,7 @@ valid_cols = [
 'capital-loss', 'hours-per-week', 'target', 'set'
 ]
 
-assert len(list_col_names_to_encode) + len(valid_cols) == len(full_data_clean.columns), 'not true'
+assert len(list_col_names_to_encode) + len(valid_cols) == len(full_data_clean.columns) - 1, 'not true' # - 1 -> target
 
 # Loop function over relevant columns
 for col in list_col_names_to_encode:
@@ -105,7 +91,7 @@ engineered_df = pd.concat(list(engineered_df_d.values()),axis=1)
 df = pd.concat([engineered_df,full_data_clean[valid_cols]],axis=1)
 X = df.drop(columns='target')
 y = df['target']
-label_map = {'<=50K': 0, '>50K': 1}
+label_map = {0: [' <=50K', ' <=50K.'], 1: [' >50K', ' >50K.']}
 y = y.apply(lambda x: 0 if x == '<=50K' else 1).reset_index(drop=True)
 os.makedirs('split-data/processed', exist_ok=True)
 with open('split-data/processed/label_map.json', 'w') as f:
@@ -113,15 +99,6 @@ with open('split-data/processed/label_map.json', 'w') as f:
 bool_musk = (df['set'] == 'train').values
 X_tr, X_ts = X.loc[bool_musk].drop('set', axis=1), X.loc[~bool_musk].drop('set', axis=1)
 y_tr, y_ts = y[bool_musk], y[~bool_musk]
-
-### TO SCALE - NOT NEEDED FOR TREE METHODS ####
-# continuous_features = ['fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week'] # continuous features
-# X_tr[continuous_features] = X_tr[continuous_features].astype(float)
-# X_ts[continuous_features] = X_tr[continuous_features].astype(float)
-# scaler = StandardScaler()
-# scaler.fit(X_tr[continuous_features]) # Fit only on X_tr continuous columns
-# X_tr.loc[:, continuous_features] = scaler.transform(X_tr[continuous_features])
-# X_ts.loc[:, continuous_features] = scaler.transform(X_ts[continuous_features])
 
 X_tr.to_csv('split-data/processed/X_tr.csv',index=False)
 X_ts.to_csv('split-data/processed/X_ts.csv',index=False)
